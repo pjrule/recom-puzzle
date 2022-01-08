8690739d6c4913a017595a4cfb16e3bb65eb04c5
Parker J. Rule <parker.rule@tufts.edu>
2022-01-08 02:21:36 -0500

    Initial attempt at grid

--- src/mggg/recom_puzzle.cljs
+++ src/mggg/recom_puzzle.cljs
@@ -1,22 +1,78 @@
+;; UI for in-browser ReCom puzzle.
+;;
+;; some helpers inspired by (or borrowed from) the learn-cljs tutorials
+;; see https://github.com/kendru/learn-cljs/blob/main/code/lesson-20/contacts/
+;;     src/learn_cljs/contacts.cljs
 (ns ^:figwheel-hooks mggg.recom-puzzle
-  (:require
-   [goog.dom :as gdom]))
-
-(println "This text is printed from src/mggg/recom_puzzle.cljs. Go ahead and edit it and see reloading in action.")
-
-(defn multiply [a b] (* a b))
-
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
-
-(defn get-app-element []
-  (gdom/getElement "app"))
+  (:require-macros [hiccups.core :as hiccups])
+  (:require [hiccups.runtime]
+            [goog.dom :as gdom]
+            [goog.events :as gevents]
+            [goog.string :as str]))
 
 
+;; Container setup.
+(def app-container (gdom/getElement "app"))
+(defn set-app-html! [html-str]
+  (set! (.-innerHTML app-container) html-str))
+(declare refresh!)
 
-;; specify reload hook with ^:after-load metadata
-(defn ^:after-load on-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+
+;; Grid parameters (TODO: add menu options, load initial plans from server)
+(def width 8)
+(def height 8)
+
+(def initial-state
+  {:grid (map (fn [row] (repeat width row)) (range height))
+   :selected #{}})
+
+(def cases
+  (map (fn [n] {:name (str/format "%dx%d → %d, no tolerance" n n n)
+                :path (str/format "%d_%d_%d_0.json" n n n)})
+       (range 4 11)))
+
+;; Grid rendering and event handling.
+(defn render-grid-row [row-idx row-state]
+  (map-indexed
+    (fn [col-idx assignment]
+      [:button {:id (str "grid-cell-" row-idx "-" col-idx)
+                :class (str "grid-cell dist-" assignment)}
+        (+ 1 assignment)])
+    row-state))
+
+(defn render-grid [grid-state]
+  (map-indexed
+    (fn [row-idx row-state]
+      [:div {:data-grid-row row-idx :class "grid-row"}
+       (render-grid-row row-idx row-state)])
+    grid-state))
+
+(defn on-dist-click [dist])
+
+(defn attach-grid-events! [grid-state]
+  (map-indexed
+    (fn [row-idx row-state] 
+      (map-indexed
+        (fn [col-idx assignment]
+          (when-let
+            [grid-button (gdom/getElement (str "grid-cell-" row-idx "-" col-idx))]
+              (gevents/listen grid-button "click"
+                              (fn [_] (on-dist-click assignment)))))))
+    grid-state))
+
+
+;; App rendering.
+(defn attach-event-handlers! [state]
+  (attach-grid-events! (get state :grid)))
+
+(defn render-app! [state]
+  (set-app-html!
+   (hiccups/html
+    [:div {:class "app-main"}
+     (render-grid (get state :grid))])))
+
+(defn refresh! [state]
+  (render-app! state)
+  (attach-event-handlers! state))
+
+(refresh! initial-state)
